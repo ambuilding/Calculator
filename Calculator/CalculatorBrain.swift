861ab50615ae7a79b6c7355def560745c8cef5ca
ambuilding <wangqi@172-13-1-68.lightspeed.mssnks.sbcglobal.net>
2016-06-29 16:54:16 +0800

    AutoLayout

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,113 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by WangQi on 16/5/15.
+//  Copyright © 2016年 WangQi. All rights reserved.
+//
+
+import Foundation
+
+//func multiply(op1: Double, op2: Double) -> Double {
+//    return op1 * op2
+//}
+
+class CalculatorBrain {
+    
+    private var accumulator = 0.0
+    var internalPromgram = [AnyObject]()
+    
+    func setOperand(operand: Double) {
+        accumulator = operand
+        internalPromgram.append(operand)
+    }
+    
+    func addUnaryOperation(symbol: String, operation: (Double) -> Double) {
+        operations[symbol] = Operation.UnaryOperation(operation)
+    }
+    
+    private var operations: Dictionary<String, Operation> = [
+    "π" : Operation.Constant(M_PI),
+    "e" : Operation.Constant(M_E),
+    "±" : Operation.UnaryOperation({ -$0 }),
+    "√" : Operation.UnaryOperation(sqrt),
+    "cos" : Operation.UnaryOperation(cos),
+    "×" : Operation.BinaryOperation({ $0 * $1 }), // closure
+    "÷" : Operation.BinaryOperation({ $0 / $1 }),
+    "+" : Operation.BinaryOperation({ $0 + $1 }),
+    "-" : Operation.BinaryOperation({ $0 - $1 }),
+    "=" : Operation.Equals
+    ]
+    
+    private enum Operation {
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double, Double) -> Double)
+        case Equals
+    }
+    
+    func performOperation(symbol: String) {
+        internalPromgram.append(symbol)
+        if let operation = operations[symbol] {
+            switch operation {
+            case .Constant(let associatedConstantValue):
+                accumulator = associatedConstantValue
+            case .UnaryOperation(let function):
+                accumulator = function(accumulator)
+            case .BinaryOperation(let function):
+                executePendingBinaryOperation()
+                pending = pendingBinaryOperationInfo(binaryFunction: function, firstOprand: accumulator)
+            case .Equals:
+                executePendingBinaryOperation()
+            }
+        }
+    }
+    
+    private func executePendingBinaryOperation() {
+        if pending != nil {
+            accumulator = pending!.binaryFunction(pending!.firstOprand, accumulator)
+            pending = nil
+        }
+    }
+    
+    private var pending: pendingBinaryOperationInfo?
+    
+    struct pendingBinaryOperationInfo {
+        var binaryFunction: (Double, Double) -> Double
+        var firstOprand: Double
+    }
+    
+    // The aliased name can be used instead of the existing type everywhere in the program.
+    typealias PropertyList = AnyObject
+    
+    var program: PropertyList {
+        get {
+            return internalPromgram
+        }
+        set {
+            clear()
+            if let arrayOfOps = newValue as? [AnyObject] {
+                for op in arrayOfOps {
+                    if let operand = op as? Double {
+                        setOperand(operand)
+                    } else if let operation = op as? String {
+                        performOperation(operation)
+                    }
+                }
+            }
+        }
+    }
+    
+    func clear() {
+        accumulator = 0.0
+        pending = nil
+        internalPromgram.removeAll()
+    }
+    
+    // read-only property
+    var result: Double {
+        get {
+            return accumulator
+        }
+    }
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,18 +8,85 @@
 
 import UIKit
 
+var calculatorCount = 0
+
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var display: UILabel!
+    
+    private var userIsInTheMiddleOfTyping = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        calculatorCount += 1
+        print("Loaded up a new calculator (count = \(calculatorCount))")
+        
+        brain.addUnaryOperation("z") { [ unowned me = self ] in
+            me.display.textColor = UIColor.redColor()
+            return sqrt($0)
+        }
+    }
+    
+    deinit {
+        calculatorCount -= 1
+        print("Calculator left the heap (count = \(calculatorCount))")
+    }
+    
+    @IBAction private func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+    }
+    
+    //Computed properties don’t actually store a value. They  provide a getter and an optional setter to retrieve(get { }) and set other properties and values indirectly(set { }).
+    private var displayValue: Double {
+        get {
+            return Double(display.text!)!
+            // If Double("Hello")
+        }
+        set {
+            display.text = String(newValue)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private var brain = CalculatorBrain()
+    
+    var savedProgram: CalculatorBrain.PropertyList?
+    
+    @IBAction func save() {
+        savedProgram = brain.program
     }
-
-
+    
+    @IBAction func restore() {
+        if savedProgram != nil {
+            brain.program = savedProgram!
+            displayValue = brain.result
+        }
+    }
+    
+    @IBAction private func performOperation(sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        if let  mathematicalSymbol = sender.currentTitle {
+            brain.performOperation(mathematicalSymbol)
+        }
+        displayValue = brain.result
+    }
+    
 }
 
+/* 
+ touchDigit from 0 to 9;
+ Display the digit which you touch
+ Force unwrapped
+ If let optional binding
+ 
+ 
+ */
